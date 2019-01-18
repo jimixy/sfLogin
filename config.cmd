bc19cbdf43ff909547292f87658c292f2c478d03
jimi <m18824244884@163.com>
2019-01-18 13:45:20 +0800

    Update config.cmd

--- config.cmd
+++ config.cmd
@@ -1,1 +1,8 @@
 
+@IF EXIST "%~dp0\node.exe" (
+  "%~dp0\node.exe"  "%~dp0\.\defaultConfig\config.js" %*
+) ELSE (
+  @SETLOCAL
+  @SET PATHEXT=%PATHEXT:;.JS;=;%
+  node  "%~dp0\.\defaultConfig\config.js" %*
+)
